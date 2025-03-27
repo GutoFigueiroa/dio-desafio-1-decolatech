d8e32c75130a75d25cca4d2b0bb1f283ef40bdd9
GutoFigueiro <fernando.figueiroa.pe@gmail.com>
2025-03-27 15:25:08 -0300

    Initial commit

---
+++ dio-desafio-1-decolatech/src/main/java/dio/dio_desafio_1_decolatech/DioDesafio1DecolatechApplication.java
@@ -0,0 +1,13 @@
+package dio.dio_desafio_1_decolatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafio1DecolatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafio1DecolatechApplication.class, args);
+	}
+
+}
---
+++ dio-desafio-1-decolatech/src/test/java/dio/dio_desafio_1_decolatech/DioDesafio1DecolatechApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio_desafio_1_decolatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafio1DecolatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
